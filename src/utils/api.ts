a26c8b7d6bcb276c15546d3317f3fd0a8059c01b
Anurag Chhetri <anurag.chhetri@nagarro.com>
2024-12-26 14:38:32 +0530

    added pwa

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,15 @@
-import axios from 'axios';
-export const baseURL="https://text-sharing-backend.vercel.app"
+import axios from "axios";
+// export const baseURL="https://text-sharing-backend.vercel.app"
 
-// export const baseURL="http://localhost:5001"
+export const baseURL =
+  import.meta.env.MODE == "development"
+    ? "http://localhost:5001"
+    : "https://text-sharing-backend.vercel.app";
 
 const api = axios.create({
   baseURL: baseURL,
   headers: {
-    'Content-Type': 'application/json', // Set default content type
+    "Content-Type": "application/json", // Set default content type
   },
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,65 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+// Define the manifest file with the correct type
+const manifestFile: Partial<VitePWAOptions> = {
+  registerType: "prompt", // this is valid
+  includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+  manifest: {
+    name: "My PWA App",
+    short_name: "My PWA",
+    description: "My amazing Progressive Web App!",
+    icons: [
+      {
+        src: "/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#181818",
+    background_color: "#e0cc3b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      ...manifestFile,
+      devOptions: {
+        enabled: true, // Ensure PWA is enabled in development mode
+        // You can add more options if needed
+      },
+    }),
+  ],
+});
