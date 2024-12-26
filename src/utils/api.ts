1047c5ee26f621d528b0bcbf5134737d95f0848c
Anurag Chhetri <anurag.chhetri@nagarro.com>
2024-12-26 13:00:20 +0530

    text-sharing_frontend

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// export const baseURL="https://text-sharing-backend.vercel.app"
+export const baseURL="https://text-sharing-backend.vercel.app"
 
-export const baseURL="http://localhost:5001"
+// export const baseURL="http://localhost:5001"
 
 const api = axios.create({
   baseURL: baseURL,
